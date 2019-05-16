da63c9dd3311a1f49ace06485a529a288369d1fa
Roi ben haim <roi.benhaim@gmail.com>
2019-05-16 22:39:38 +0300

    no message

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 
 import * as MethodusImport from './shim';
+export * from './shim';
 export const Methodus = MethodusImport;
--- src/transports/rest/index.ts
+++ src/transports/rest/index.ts
@@ -1,13 +1,13 @@
 
 import { Request } from './Request';
 import { MethodError } from '../../response';
-export function send(methodus: any, functionArgs: any, paramsMap: any, securityContext: any) {
+export function send(methodus: any, functionArgs: any, paramsMap: any, securityContext: any): Promise<any> {
     const request = new Request();
     const baseUrl = methodus.resolver();
     if (baseUrl) {
         return request.sendRequest(methodus.verb, baseUrl + methodus.route, functionArgs,
-            paramsMap, securityContext);
+            paramsMap, securityContext) as any;
     } else {
-        return new MethodError('no server found for this method' + methodus.route, 302);
+        throw new MethodError('no server found for this method' + methodus.route, 302);
     }
 }
