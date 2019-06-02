f2f4109ad57f3bd71b173d32b7ada1641721bda6
Roi ben haim <roi.benhaim@gmail.com>
2019-06-02 10:18:19 +0300

    revert to shim export

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,5 @@
 
 import * as MethodusImport from './shim';
-
-export * from './verbs';
-export * from './log';
-export * from './interfaces/methodus';
-export * from './decorators';
-export * from './method/index';
-export * from './response';
-export * from './params';
-export * from './config';
-export * from './class-container';
-export * from './server';
-export * from './servers';
-export * from './server.configured';
-export * from './builtin';
-export * from './mocker';
+export * from './shim';
 
 export const Methodus = MethodusImport;
