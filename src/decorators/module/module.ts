33e50df5d0e77a491c4c9ee95f12e4102a300411
NodulusTeam <nodulusteam@gmail.com>
2020-01-22 09:16:18 +0200

    no message

--- src/decorators/module/module.ts
+++ src/decorators/module/module.ts
@@ -3,10 +3,14 @@ import 'reflect-metadata';
 
 /** the Module decorator registers a module
  */
-export function Module() {
+export function Module(name?: string) {
     return (target: any) => {
         const original = target.prototype.constructor;
         original.prototype.options = original.prototype.options ||
             { servers: [], classes: [], clients: [], plugins: [] };
+            
+        if (name) {
+            original.prototype.options.name = name;
+        }
     };
 }
