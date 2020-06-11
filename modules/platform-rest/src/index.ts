4d4f4b55b8424590a760c24fe31d80f9c7683cef
NodulusTeam <nodulusteam@gmail.com>
2020-06-12 00:36:26 +0300

    no message

--- modules/platform-rest/src/index.ts
+++ modules/platform-rest/src/index.ts
@@ -1,5 +1,5 @@
 import * as plugin from './sender';
-export const Http = { name: 'Http', class: plugin };
 export * from './web-request';
 export * from './interfaces';
 export const name: string = 'Http/Rest';
+export const Http = { name: 'Http', class: plugin };
