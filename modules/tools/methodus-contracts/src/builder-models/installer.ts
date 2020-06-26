f773ae6f06f07d9a796ad9fdcf0f8cb341634f79
NodulusTeam <nodulusteam@gmail.com>
2020-06-26 14:42:27 +0300

    no message

--- modules/tools/methodus-contracts/src/builder-models/installer.ts
+++ modules/tools/methodus-contracts/src/builder-models/installer.ts
@@ -1,4 +1,4 @@
-import shelljs from 'shelljs';
+import * as shelljs from 'shelljs';
 import * as exec from 'shelljs.exec';
 
 const LINE = '----------------------------------------------------------------------';
