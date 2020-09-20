03ef5065b2ca991e6ff5c757f19e37473ec2c5be
NodulusTeam <nodulusteam@gmail.com>
2020-09-20 20:51:13 +0300

    no message

--- modules/tools/methodus-contracts/src/index.ts
+++ modules/tools/methodus-contracts/src/index.ts
@@ -1,1 +1,2 @@
-throw(new Error(`This module is designed to work as a utility and should not be required.`));
+throw (new Error(`This module is designed to work as a 
+utility and should not be required.`));
