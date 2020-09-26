5fcadcf2503aec1e2ee20fe4c6b4ee8b78ce3791
NodulusTeam <nodulusteam@gmail.com>
2020-09-26 16:48:51 +0300

    no message

--- modules/tests/integration-test/src/test.spec.ts
+++ modules/tests/integration-test/src/test.spec.ts
@@ -2,6 +2,7 @@ import { AppModule } from './app-module';
 
 
 describe('test the AppModule', ()=>{
+    jest.setTimeout(10);
     let appModule:AppModule;
     afterAll(()=>{
         appModule.kill();
